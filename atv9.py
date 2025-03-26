41cf2f9bc8444db7357619b3a39adffde22e5781
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:36:24 -0300

    Update atv9.py

--- atv9.py
+++ atv9.py
@@ -5,9 +5,9 @@ num3 = int(input("Diga mais outro numero: "))
 numeros = [num1, num2, num3]
 
 def bubble_sort_desc(lista):
-    n = len(lista)
-    for i in range(n - 1):  
-        for j in range(n - 1 - i):  
+    num = len(lista)
+    for i in range(num - 1):  
+        for j in range(num - 1 - i):  
             if lista[j] < lista[j + 1]:  
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
 
