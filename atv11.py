20e56340e5cc9ea88d32870b74e0d7af88c9fcb8
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:24:30 -0300

    Update atv11.py

--- atv11.py
+++ atv11.py
@@ -1,11 +1,9 @@
-#Crie um sistema de login que não permite que o nome de usuário e a senha sejam iguais.
-
-user = input("Informe seu login: ")
+usuario = input("Informe seu login: ")
 senha = input("Informe sua senha: ")
 
-while user == senha:
+while usuario == senha:
     print("Sua senha e login nao podem ser iguais, tente de novo.")
-    user = input("Informe seu login: ")
+    usuario = input("Informe seu login: ")
     senha = input("Informe sua senha: ")
 
 else:
