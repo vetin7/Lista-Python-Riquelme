61fc4dad830083df4be65595200534d107dff49f
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:35:32 -0300

    Update atv6.py

--- atv6.py
+++ atv6.py
@@ -1,8 +1,8 @@
 def analise_primo(n):
-    if n < 2:
+    if num < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
             return False
     return True
 
