8c5d465610554a57ea95ee3b1386a0b1e93e4970
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:22:35 -0300

    Update atv1.py

--- atv1.py
+++ atv1.py
@@ -1,17 +1,12 @@
-#Peça ao usuário três notas e calcule:
-#a. A média aritmética simples
-#b. A média ponderada considerando os pesos 2, 2 e 3
-#c. A média ponderada considerando os pesos 1, 2 e 2
+num1 = int(input("Diga o primeiro numero: "))
+num2 = int(input("Diga o segundo numero: "))
+num3 = int(input("Diga o terceiro numero: "))
 
-n1 = int(input("Diga o primeiro numero: "))
-n2 = int(input("Diga o segundo numero: "))
-n3 = int(input("Diga o terceiro numero: "))
+media_arit = (num1 + num2 + num3) / 3
 
-media_arit = (n1 + n2 + n3) / 3
+media_pond1 = (num1 * 2), (num2 * 2), (num3 * 3)
 
-media_pond1 = (n1 * 2), (n2 * 2), (n3 * 3)
-
-media_pond2 = (n1 * 1), (n2 * 2), (n2 * 2)
+media_pond2 = (num1 * 1), (num2 * 2), (num2 * 2)
 
 print("Media aritmetica:", media_arit)
 print("Media ponderada peso 2, 2 e 3", media_pond1)
