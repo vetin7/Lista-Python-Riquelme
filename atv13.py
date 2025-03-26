26b14a6a560dc484afad58927611aceedbb2f56f
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:27:14 -0300

    Update atv13.py

--- atv13.py
+++ atv13.py
@@ -1,11 +1,8 @@
-#Dado um salário inicial e um aumento percentual que dobra a cada ano, calcule o
-#salário atual após vários anos.
-
-salario_inicial = int(input("Informe seu salario inicial: "))
+primeiro_salario = int(input("Informe seu primeiro salario : "))
 tempo = int(input("Informe o tempo que voce trabalha: "))
 
 
-salario_final = salario_inicial * (2 ** tempo)
+salario_final = primeiro_salario * (2 ** tempo)
 
 
 print(f"Seu salario apos {tempo} anos de servico sera: {salario_final}.")
