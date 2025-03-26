0b2610084648f16da3ff52dcf91137c87e6000cf
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:23:41 -0300

    Update atv10.py

--- atv10.py
+++ atv10.py
@@ -1,9 +1,7 @@
-#Solicite um número inteiro maior que 1 e verifique se ele é primo.
-
-n = int(input("Diga um numero inteiro maior que 1: "))
+num = int(input("Diga um numero inteiro maior que 1: "))
 
 primo = True
-for i in range(2, int(n**0.5) + 1):
+for i in range(2, int(num**0.5) + 1):
         if n % i == 0:
             primo = False
             break
