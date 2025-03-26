67888af17d744dc1772c335198833d4ec40e2b09
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:28:51 -0300

    Update atv3.py

--- atv3.py
+++ atv3.py
@@ -1,11 +1,7 @@
-#Peça ao usuário o valor total das mercadorias compradas. Se for menor que R$500,
-#não há imposto. Caso contrário, aplique uma taxa de 50% sobre o valor que ultrapassar
-#os R$500.
+valor_total = int(input("Diga o valor total das compras:"))
 
-valor = int(input("Diga o valor das compras:"))
-
-if valor > 500:
-    taxa = (valor - 500) * 1.5
+if valor_total > 500:
+    taxa = (valor_total - 500) * 1.5
     valor_taxado = taxa + 500
     print(f"Valor acima de R$500. Valor com taxa de 50%: {valor_taxado}")
 
