374b89b7f5b921d75d45cc7adb1c3ef2c79efe57
vetin7 <overgames32661080@gmail.com>
2025-03-26 13:25:38 -0300

    Update atv10.py

--- atv10.py
+++ atv10.py
@@ -2,7 +2,7 @@ num = int(input("Diga um numero inteiro maior que 1: "))
 
 primo = True
 for i in range(2, int(num**0.5) + 1):
-        if n % i == 0:
+        if num % i == 0:
             primo = False
             break
           
